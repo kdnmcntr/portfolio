e14f9d2b316917df2ba20f6220464a8dae1ebaaf
Kaden McEntire <122327533+kdnmcntr@users.noreply.github.com>
2026-03-08 21:16:08 -0600

    updated home and about pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,9 +6,21 @@ const About = () => {
     <div className="pt-4">
         <h1 className="h1">About me</h1>
     </div>
-    <div>
-        <img  className={styles.pfp} src={picture}/>
+    <br/>
+    <div className={styles.container}>
+        <div className="p-3">
+            <img  className={styles.pfp} src={picture}/>
+        </div>
+        <div className="p-3 fs-4">
+            <p>
+                I am a computer science student at Weber State University. I like programming and learning about computers in general. 
+                I have experience programming in Java, C, C++, C#, Python, Rust, and other programming languages.
+                I am also a classically trained pianist with over 21 years of experience. 
+                I enjoy learning foreign languages and am fluent in Spanish. I have also learned a small amount of French, German, and Turkish.
+            </p>
+        </div>
     </div>
+    
     </>);
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,12 @@ const Home = () => {
     return(<>
     <div className="pt-4">
         <h1 className="h1">Welcome</h1>
+        <br/>
+        <div className="fs-3">
+            <p>Welcome to my website!</p>
+            <p>Here you can find a list of projects on which I've worked as well my resume, LinkedIn profile, and some other information about me.</p>
+            <p>Also included is a list of my piano repetoire. </p>
+        </div>
     </div>
     </>);
 }
